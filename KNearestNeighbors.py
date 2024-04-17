130a21a16f766b2eed1e1a72a275e7c094935fc0
Cynthia Xiong <cynthia.xiong@outlook.com>
2024-04-17 12:48:20 -0400

    Simplify code

--- KNearestNeighbors.py
+++ KNearestNeighbors.py
@@ -13,36 +13,34 @@ class KNearestNeighbors:
         return
 
 
-    def get_nearest_neighbors(self, X:torch.Tensor, n_neighbors=5):
+    def get_nearest_neighbors(self, X:torch.Tensor, n_neighbors=5, return_idx=False):
         '''
         Use training data to calculate k-nearest neighbors for each sample observation.
 
         Args:
             X: Sample data
             n_neighbors: The number of neighbors to count for
+            return_idx: Return the indices of the nearest neighbors in X if False; otherwise return the original values
         '''        
         # Run through each observation in the sample data
-        nearest_neighbors = [] # Store neighbors nearest to each sample observation
-        for sample_obs in X:
+        nearest_neighbors = torch.zeros(size=(len(X), n_neighbors)) # Store neighbors nearest to each sample observation
+        for sample_i, sample_obs in enumerate(X):
             # Compute distances between the current sample observation and every observation in the training data
-            neighbors = [] # Store neighbors nearest to the sample observation
-            dists = [] # Store distances of the nearest neighbors
-            for train_obs in self.X:
-                dist = self.__dist(sample_obs, train_obs)
-                if len(dists) < n_neighbors:
-                    neighbors.append(train_obs)
-                    dists.append(dist)
-                elif dist < max(dists):
-                    # Replace furthest training observation
-                    idx = dists.index(max(dists))
-                    neighbors[idx] = train_obs.tolist()
-                    dists[idx] = dist
-            nearest_neighbors.append(neighbors)
+            dists = [0] * len(self.X)
+            for train_i, train_obs in enumerate(self.X):
+                 dists[train_i] = self.__dist(sample_obs, train_obs)
+            # Get indices of nearest neighbors
+            neighbors = torch.argsort(torch.tensor(dists))[:n_neighbors]
+            nearest_neighbors[sample_i] = neighbors
         
-        return torch.Tensor(nearest_neighbors)
-
+        if return_idx:
+            return nearest_neighbors
+        else:
+            nearest_neighbors = nearest_neighbors.int()
+            return self.X[nearest_neighbors]
+        
                 
     def __dist(self, u, v):
         '''Computes Euclidean distance'''
-        return torch.sqrt(torch.sum( torch.Tensor([(u[i]-v[i])**2 for i in range(v.shape[0])]) ))
+        return torch.sqrt(torch.sum( torch.tensor([(u[i]-v[i])**2 for i in range(v.shape[0])]) ))
     
